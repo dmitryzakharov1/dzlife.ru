70b16dabf7f514256956bec2c0064dd022bbeb37
dmitryzakharov1 <zaharov.dmitrii@gmail.com>
2022-06-20 20:17:23 +0300

    update ip

--- nezumi.org.ru.lua
+++ nezumi.org.ru.lua
@@ -1,4 +1,4 @@
-aaaa(_a, "2a02:2168:8749:9700:9ce0:f2b9:209a:d76e")
+aaaa(_a, "2a02:2168:8680:5300:7dab:5cfa:cacf:7b43")
 cname("www", _a)
 mx(_a, "mx.yandex.net.", 21600)
 txt(_a, "v=spf1 redirect=_spf.yandex.net")
