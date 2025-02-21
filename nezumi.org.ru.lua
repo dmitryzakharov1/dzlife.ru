4b922140873d11c821cf8cca677474eb1d114ea0
dmitryzakharov1 <zaharov.dmitrii@gmail.com>
2025-02-21 18:25:26 +0300

    update ip

--- nezumi.org.ru.lua
+++ nezumi.org.ru.lua
@@ -1,5 +1,6 @@
-a(_a, "")
-aaaa(_a, "")
+a(_a, "37.110.17.247")
+aaaa(_a, "fdd3:cf44:d59b::f42
+fdd3:cf44:d59b:0:ffbc:dafb:7851:4352")
 cname("www", _a)
 mx(_a, "mx.yandex.net.", 21600)
 txt(_a, "v=spf1 redirect=_spf.yandex.net")
